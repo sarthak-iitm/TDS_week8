8ef7de34632d4b717a03513a76345ec09569034d
sarthak-iitm <91659276+sarthak-iitm@users.noreply.github.com>
2023-04-14 19:42:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 import streamlit as st
+
 st.title("TDS Module 8 Assignment")
+
+# take three number inputs
 a = st.number_input("Enter first number: ")
 b = st.number_input("Enter second number: ")
-c = a + b
-st.write("The sum of both numbers is:" , c) 
+c = st.number_input("Enter third number: ")
+
+# find the largest among the three numbers
+if a >= b and a >= c:
+    largest = a
+elif b >= a and b >= c:
+    largest = b
+else:
+    largest = c
+
+st.write("The largest number among", a, ",", b, "and", c, "is:", largest)
